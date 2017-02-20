b11c16a60388fb2b3c5652f4d215c47e7662ab41
Chamala Rahul <skb386@nyu.edu>
2017-02-20 02:41:33 -0800

    Automation: Creating tfvars from config.yml

--- main.py
+++ main.py
@@ -2,8 +2,27 @@
 
 import os
 import sys
+import yaml
 
 import variables as vars
+
+def gen_tfvars(var_file):
+    try:
+        with open("config.yml", 'r') as yml_config:
+            config = yaml.load(yml_config)
+
+        tfvars_contents = ""
+        for key,val in config.items():
+            if key == "private_key_path":
+                val = os.path.abspath(val)
+            tfvars_contents = tfvars_contents + key + "=\"" + str(val) + "\"\n"
+
+        with open(var_file, 'w') as tf_writer:
+                tf_writer.write(tfvars_contents)
+        return True
+    except:
+        return False
+
 
 def run_ansible(config_dir):
     ret = os.system("python utils/gen_inventory.py")
@@ -18,11 +37,12 @@ def run_ansible(config_dir):
         sys.exit(1)
 
 def run_terraform(terraform_dir):
-    cmd = "terraform apply -var-file " + var_file + " -state " + state_file + " -state-out " + out_state_file + " " + terraform_dir
-    # cmd = "terraform destroy -var-file " + var_file + " -state " + state_file + " -state-out " + out_state_file + " " + terraform_dir
-    ret = os.system(cmd)
+    if gen_tfvars(var_file):
+        cmd = "terraform apply -var-file " + var_file + " -state " + state_file + " -state-out " + out_state_file + " " + terraform_dir
+        # cmd = "terraform destroy -var-file " + var_file + " -state " + state_file + " -state-out " + out_state_file + " " + terraform_dir
+        ret = os.system(cmd)
     if ret != 0 :
-        print("Failure while provisioning. Exiting.")
+        print("ERROR [Terraform]: Failure while provisioning. Exiting.")
         sys.exit(1)
     return True
 
--- terraform/override.tfvars
+++
@@ -1,8 +0,0 @@
-clustername="rescale"
-
-public_key_path="/Users/rahul1/.ssh/demokp.pem"
-key_name="demokp"
-instance_count = 1
-instance_type="t2.micro"
-ami_id="ami-d2c924b2"
-ssh_user="centos"
